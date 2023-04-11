9de2c45765aa9e0e18181b92f5297ae3acff569f
Dakunvarlar <dakunvarlar@gmail.com>
2023-04-11 10:47:27 -0700

    INHERITANCE

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Defines an empty class BaseGeometry"""
+"""
+   5-base_geometry module
+   Empty class BaseGeometry.
+"""
+
 
 class BaseGeometry:
-    """Represents base geometry"""
+    """Empty class BaseGeometry."""
     pass
